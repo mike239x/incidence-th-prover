9dd282df1c8d038c3dfd1a9cd918fd03b6919b4f
mike239x <mikelezhnin@gmail.com>
2018-07-12 01:22:18 +0200

    added a button to launch the proving algorithm without the button some weird bug happens, I guess some race condition or something

--- prover/prover.js
+++ prover/prover.js
@@ -2,10 +2,10 @@ function prove_collinear(A,B,C) {
 	try {
 		let state = cindy.saveState();
 		let configuration = state.geometry;
-		console.log(configuration);
+		
+
 	} catch (e) {
 		console.log(e.message);
 	} finally {
 	}
 }
-prove_collinear("A","I","D");
