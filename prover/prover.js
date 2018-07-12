e3a4ecd0a35659655cf1417ab5326cca47fdc78f
mike239x <mikelezhnin@gmail.com>
2018-07-12 13:40:58 +0200

    added logging on the screen, logging the given configuration

--- prover/prover.js
+++ prover/prover.js
@@ -1,11 +1,22 @@
+function log(text) {
+	document.getElementById("log").innerHTML+=text+'</br>';
+}
+
 function prove_collinear(A,B,C) {
 	try {
+		log(`proving that the points ${A}, ${B} and ${C} are collinear`);
 		let state = cindy.saveState();
 		let configuration = state.geometry;
-		
-
+		log('read configuration:')
+		for (let elem of configuration) {
+			if (elem.type == 'Free') {
+				log(`${elem.name} = Free point`)
+			} else {
+				log(`${elem.name} = ${elem.type}(${elem.args})`)
+			}
+		}
 	} catch (e) {
-		console.log(e.message);
+		log(e.message);
 	} finally {
 	}
 }
