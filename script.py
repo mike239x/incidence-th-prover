f1994175f46afda68ec4dd10c5cc9df4b73f1e8d
mike239x <mikelezhnin@gmail.com>
2018-07-12 13:41:41 +0200

    small edit of readme, removed script.py

--- script.py
+++
@@ -1,8 +0,0 @@
-def prove(config, select):
-    '''Generate a proof that selected points are lying on the same line.
-
-    Keyword arguments:
-    config -- configuration of points and lines in RP2.
-    select -- selectd points from the configuration.
-    '''
-    pass
